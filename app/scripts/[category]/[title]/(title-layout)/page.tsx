2f9671511e418f288461cd3d1966100d18aa037e
RRAJ644 <riturajsingh.17248@gmail.com>
2025-01-11 22:03:21 +0530

    Title page

--- app/scripts/[category]/[title]/(title-layout)/page.tsx
+++ app/scripts/[category]/[title]/(title-layout)/page.tsx
@@ -1,24 +1,26 @@
-import DataScript from '@/components/shared/DataScript';
-import InfoBar from '@/components/shared/InfoBar';
-import { Metadata } from 'next';
+import DataScript from '@/components/shared/DataScript'
+import InfoBar from '@/components/shared/InfoBar'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Script Details',
   description: 'Detailed information about the selected script',
-};
-
-interface PageProps {
-  params: {
-    category: string;
-    title: string;
-  };
 }
 
-export default function TitlePage({ params }: PageProps) {
+interface PageProps {
+  params: Promise<{
+    category: string
+    title: string
+  }>
+}
+
+export default async function TitlePage({ params }: PageProps) {
+  const resolvedParams = await params
+
   return (
-    <section className="flex justify-between gap-x-20 border-2 border-green-500 w-full">
+    <section className='flex justify-between gap-x-20 border-2 border-green-500 w-full'>
       <InfoBar />
       <DataScript />
     </section>
-  );
+  )
 }
