d501e1c47affc99f5a9027942a430ffe4f0db0a6
RRAJ644 <riturajsingh.17248@gmail.com>
2025-01-12 18:42:10 +0530

    removal of inner-page design

--- app/scripts/[category]/[title]/(title-layout)/page.tsx
+++ app/scripts/[category]/[title]/(title-layout)/page.tsx
@@ -1,5 +1,3 @@
-import DataScript from '@/components/shared/DataScript'
-import InfoBar from '@/components/shared/InfoBar'
 import { SCRIPT_DATA } from '@/constants/constants'
 import { extractTabs } from '@/lib/utils'
 import { Metadata } from 'next'
@@ -17,14 +15,8 @@ interface PageProps {
 }
 export default async function TitlePage({ params }: PageProps) {
   const resolvedParams = await params
-  const tabs = extractTabs(SCRIPT_DATA)
-  const script = SCRIPT_DATA[0].script
 
   return (
-    <section className='flex gap-6 p-6 min-h-screen border-2 w-full'>
-      <InfoBar tabs={tabs} />
-      <div className='border-l h-full border-neutral-300 border rounded-lg'></div>
-      <DataScript script={script} />
-    </section>
+    <section className='flex gap-6 p-6 min-h-screen border-2 w-full'></section>
   )
 }
